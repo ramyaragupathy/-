48b1c7be65bf0538dccf2d6ea2054493e98999cc
Ramya <ramya@mapbox.com>
2017-12-22 10:31:27 +0530

    boolean, reassignment

--- python/BeginnerLevel/numbers&operators.py
+++ python/BeginnerLevel/numbers&operators.py
@@ -1,8 +1,8 @@
 print(3+1) # computes and prints the value
 3+1 # does not print the value
 print(3**2) # 3 to the power of 2
-print(1+2+3*3) # without parenthesis
-print((1+2+3)*3) # with parenthesis
+print(1+2+3*3) # without parentheses
+print((1+2+3)*3) # with parentheses
 print(9%2) #modulo operation
 print(15//2) # integer division
 print(-5//2) # integer division
@@ -15,3 +15,21 @@ print(16/4)
 #type conversion
 print(int(49.7))
 print(float(3520+3239))
+
+#reassignment operators
+x = 5
+x += 5
+x *= 5
+x -= 5
+x /= 5
+print(x)
+
+#Using multiple assignment
+x, y = 514.86, 320.51
+print(x,y)
+
+#Booleans & Comparison operators
+print(1>2)
+print(1<2)
+x = True
+print(x)
