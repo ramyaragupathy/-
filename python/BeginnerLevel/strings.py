da53d8147d1e5e88cd66487d7b091382c87ca828
Ramya <ramya@mapbox.com>
2017-12-22 10:43:19 +0530

    reorganise comment

--- python/BeginnerLevel/strings.py
+++ python/BeginnerLevel/strings.py
@@ -12,11 +12,10 @@ print(messiah)
 pet_halibut = 'Why should I be tarred with the epithet "loony" merely because I have a pet halibut?'
 print(pet_halibut)
 
-username = "Kinari"
-timestamp = "04:50"
-url = "http://petshop.com/pets/mammals/cats"
-
 # TODO: print a log message incorporating the strings above.
 # The message should be use the same format as this one:
 # "Yogesh accessed the site http://petshop.com/pets/reptiles/pythons at 16:20."
+username = "Kinari"
+timestamp = "04:50"
+url = "http://petshop.com/pets/mammals/cats"
 print(username + " accessed the site " + url + " at " + timestamp + ".")
