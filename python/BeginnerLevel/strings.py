dbb1b9fd2facef34aa9cb6f6533066995235a888
Ramya <ramya@mapbox.com>
2017-12-22 10:54:22 +0530

    type conversions while concatenating

--- python/BeginnerLevel/strings.py
+++ python/BeginnerLevel/strings.py
@@ -33,3 +33,18 @@ name_length = len(given_name +" "+ middle_names + " "+family_name)
 
 driving_licence_character_limit = 28
 print(name_length <= driving_licence_character_limit)
+
+# Types & type conversions
+mon_sales = "121"
+tues_sales = "105"
+wed_sales = "110"
+thurs_sales = "98"
+fri_sales = "95"
+
+#TODO: Print a string with this format: This week's total sales: xxx
+
+total_sales = int(mon_sales) + int(tues_sales)  + int(wed_sales) + int(thurs_sales) + int(fri_sales)
+# Note that concatenation operator works only on strings
+# print("This week's total sales: " + total_sales) will throw an error
+# In JS you need ntoo worry about the type conversion
+print("This week's total sales: " + str(total_sales))
