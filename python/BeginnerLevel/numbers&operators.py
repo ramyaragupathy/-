d9cc502e42f89a22f40f6081f3592b75fa3bc048
Ramya <ramya@mapbox.com>
2017-12-22 10:18:45 +0530

    Add files via upload

---
+++ python/BeginnerLevel/numbers&operators.py
@@ -0,0 +1,17 @@
+print(3+1) # computes and prints the value
+3+1 # does not print the value
+print(3**2) # 3 to the power of 2
+print(1+2+3*3) # without parenthesis
+print((1+2+3)*3) # with parenthesis
+print(9%2) #modulo operation
+print(15//2) # integer division
+print(-5//2) # integer division
+
+# Even if one integer divides another exactly, the result will be a float.
+# This behvaious is applicable only in Python 3
+# In Python 2 division by default is integer division
+print(16/4) 
+
+#type conversion
+print(int(49.7))
+print(float(3520+3239))
